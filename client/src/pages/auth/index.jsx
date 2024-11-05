17ac3704235ce4a6dc8ee42cf85f4e8c32dbf91a
Hanzila Rasool <muhammadhanzilarasool@gmail.com>
2024-11-05 02:30:56 -0800

    Courses /Lms project completion 1.0.0

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -51,8 +51,8 @@ function AuthPage() {
     <div className="flex flex-col min-h-screen">
       <header className="px-4 lg:px-6 h-14 flex items-center border-b">
         <Link to={"/"} className="flex items-center justify-center">
-          <GraduationCap className="h-8 w-8 mr-4" />
-          <span className="font-extrabold text-xl">LMS LEARN</span>
+        
+          <span className="font-extrabold text-xl">E-Courses</span>
         </Link>
       </header>
       <div className="flex items-center justify-center min-h-screen bg-background">
